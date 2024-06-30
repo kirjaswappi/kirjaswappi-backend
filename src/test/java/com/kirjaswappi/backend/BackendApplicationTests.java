8754504797686b73a02877316f360c077f40d0d3
mahiuddinalkamal <mahiuddin.al.kamal@sap.com>
2024-06-30 20:57:06 +0200

    spotless

--- src/test/java/com/kirjaswappi/backend/BackendApplicationTests.java
+++ src/test/java/com/kirjaswappi/backend/BackendApplicationTests.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2024 KirjaSwappi or KirjaSwappi affiliate company. All rights reserved.
+ * Author: Mahiuddin Al Kamal <mahiuddinalkamal>
+ */
 package com.kirjaswappi.backend;
 
 import org.junit.jupiter.api.Test;
@@ -6,8 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
