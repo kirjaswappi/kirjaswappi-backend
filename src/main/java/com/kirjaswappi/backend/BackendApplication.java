18a3c4ed03ceded085555e2a1d52966eb3656576
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2024-06-30 21:02:12 +0200

    Add PR Pipeline (#4)
    * Add PR Pipeline
    
    * ignored .idea
    
    * added dependency
    
    * spotless
    
    * updated .gitignore
    
    ---------
    
    Co-authored-by: mahiuddinalkamal <mahiuddin.al.kamal@sap.com>

--- src/main/java/com/kirjaswappi/backend/BackendApplication.java
+++ src/main/java/com/kirjaswappi/backend/BackendApplication.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2024 KirjaSwappi or KirjaSwappi affiliate company. All rights reserved.
+ * Author: Mahiuddin Al Kamal <mahiuddinalkamal>
+ */
 package com.kirjaswappi.backend;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 
 }
