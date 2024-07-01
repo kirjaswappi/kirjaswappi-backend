f12e299249f1e50cd70be5b73fe18d5747cdbbb8
mahiuddinalkamal <mahiuddin.al.kamal@sap.com>
2024-07-01 19:38:39 +0200

    added more user properties

--- src/main/java/com/kirjaswappi/backend/jpa/daos/User.java
+++ src/main/java/com/kirjaswappi/backend/jpa/daos/User.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import org.bson.types.Binary;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -20,4 +21,13 @@ public class User {
   private String id;
   private String firstName;
   private String lastName;
+  private String email;
+  private String password;
+  private String streetName;
+  private String houseNumber;
+  private String zipCode;
+  private String city;
+  private String country;
+  private String phoneNumber;
+  private Binary profilePicture;
 }
