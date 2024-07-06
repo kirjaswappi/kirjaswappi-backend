d82d51db8a087cca94e5daf5af365394ef6a0f40
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2024-07-06 17:47:29 +0200

    added new endpoint (#24)
    Co-authored-by: mahiuddinalkamal <mahiuddin.al.kamal@sap.com>

--- src/main/java/com/kirjaswappi/backend/common/utils/Constants.java
+++ src/main/java/com/kirjaswappi/backend/common/utils/Constants.java
@@ -17,4 +17,7 @@ public class Constants {
   public static final String API_DOCS = "/v3/api-docs/**";
   public static final String ACTUATOR = "/actuator";
   public static final String ROLE = "role";
+  public static final String RESET_PASSWORD = "/reset-password";
+  public static final String ID = "/{id}";
+  public static final String EMAIL = "/{email}";
 }
