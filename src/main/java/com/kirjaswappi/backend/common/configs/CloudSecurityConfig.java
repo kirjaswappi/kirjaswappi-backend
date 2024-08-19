a34cb45a96d9bf9e493bb340c2cc5d94b3f89ad8
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2024-08-19 10:12:13 +0200

    fixed cors issue (#42)
    Co-authored-by: mahiuddinalkamal <mahiuddin.al.kamal@sap.com>

--- src/main/java/com/kirjaswappi/backend/common/configs/CloudSecurityConfig.java
+++ src/main/java/com/kirjaswappi/backend/common/configs/CloudSecurityConfig.java
@@ -15,7 +15,9 @@ import static org.springframework.http.HttpMethod.DELETE;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -24,6 +26,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.kirjaswappi.backend.common.components.FilterApiRequest;
 
@@ -31,11 +36,26 @@ import com.kirjaswappi.backend.common.components.FilterApiRequest;
 @EnableWebSecurity
 @Profile("cloud")
 public class CloudSecurityConfig {
-  @Autowired
-  FilterApiRequest filterApiRequest;
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*")); // Allow all origins or customize as per your need
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")); // Allow all
+                                                                                                        // HTTP methods
+                                                                                                        // or customize
+                                                                                                        // as per your
+                                                                                                        // need
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(List.of("*")); // Allow all headers or customize as per your need
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 
   @Bean
-  public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain defaultSecurityFilterChain(FilterApiRequest filterApiRequest, HttpSecurity http)
+      throws Exception {
     return http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .csrf(crsf -> crsf.disable())
         .authorizeHttpRequests(authorize -> authorize
@@ -45,6 +65,7 @@ public class CloudSecurityConfig {
             .requestMatchers(DELETE, API_BASE + ADMIN_USERS).hasAuthority(ADMIN)
             .anyRequest().authenticated())
         .addFilterBefore(filterApiRequest, UsernamePasswordAuthenticationFilter.class)
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .build();
   }
 
