d1e2a42668031d655fc7b71af67ee22383d7919a
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2024-11-14 21:07:34 +0100

    added list of genres (#62)
    Co-authored-by: mahiuddinalkamal <mahiuddin.al.kamal@sap.com>

---
+++ src/main/java/com/kirjaswappi/backend/jpa/repositories/GenreRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 KirjaSwappi or KirjaSwappi affiliate company. All rights reserved.
+ * Author: Mahiuddin Al Kamal <mahiuddinalkamal>
+ */
+package com.kirjaswappi.backend.jpa.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.kirjaswappi.backend.jpa.daos.GenreDao;
+
+public interface GenreRepository extends MongoRepository<GenreDao, String> {
+  Optional<GenreDao> findByName(String name);
+}
