622c86d2fd0ddc90c102dd6084b758c603d81200
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2024-11-21 16:21:34 +0100

    Spotless (#68)
    * fixed profile issue
    
    * spotless
    
    * spotless
    
    ---------
    
    Co-authored-by: mahiuddinalkamal <mahiuddin.al.kamal@sap.com>

--- src/main/java/com/kirjaswappi/backend/common/configs/DatabaseConfig.java
+++ src/main/java/com/kirjaswappi/backend/common/configs/DatabaseConfig.java
@@ -5,7 +5,6 @@
 package com.kirjaswappi.backend.common.configs;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
